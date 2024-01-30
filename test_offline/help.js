569e786f94fe898acf37ad8eb2dbbd07360a000c
6210110145 <6210110145@psu.ac.th>
2024-01-30 15:05:58 +0700

    add --help process

---
+++ test_offline/help.js
@@ -0,0 +1,19 @@
+module.exports.help = () => {
+    console.log('Usage: node main_offline.js [options] <string> or <number>\n\n' + 
+                    'Options:\n' +
+                    '  -h, --help     display help for command\n' +
+                    '  -n, --name     select of name\'s air-conditioner [CentralAir, Panasonic, Samsung, Mitsubishi]\n' +
+                    '  -p, --power    ON / OFF <string>\n' +
+                    '  -m, --mode     Mode of AC <string> [COOL, DRY, FAN, AUTO]\n' +
+                    '  -t, --temp     Temperature <number> [16-30]\n' +
+                    '  -f, --fan      Speed of Fan <number> [0-3]\n' +
+                    '                 0 is AUTO\n' +
+                    '                 1 is LOW\n' +
+                    '                 2 is MIDIUM\n' +
+                    '                 3 is HIGH\n\n' +
+                    '  --swing        ON / OFF <string>\n' +
+                    '  --sleep        ON / OFF <string>\n' +
+                    '  --turbo        ON / OFF <string>\n' +
+                    '  --quiet        ON / OFF <string>\n' +
+                    '  --light        ON / OFF <string>\n')
+} 
--- test_offline/main_offline.js
+++ test_offline/main_offline.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const path_JSON = '../data/key.json';
 const { sendSignal } = require('./air.js')
+const { help } = require('./help.js')
 
 var keys = JSON.parse(fs.readFileSync(path_JSON));
 
@@ -65,23 +66,7 @@ mainOffline = () => {
     }
 
     if(argv.help) {
-        console.log('Usage: node main_offline.js [options] <string> or <number>\n\n' + 
-                    'Options:\n' +
-                    '  -h, --help     display help for command\n' +
-                    '  -n, --name     select of name\'s air-conditioner\n' +
-                    '  -p, --power    ON / OFF <string>\n' +
-                    '  -m, --mode     Mode of AC <string> [COOL, DRY, FAN, AUTO]\n' +
-                    '  -t, --temp     Temperature <number> [16-30]\n' +
-                    '  -f, --fan      Speed of Fan <number> [0-3]\n' +
-                    '                 0 is AUTO\n' +
-                    '                 1 is LOW\n' +
-                    '                 2 is MIDIUM\n' +
-                    '                 3 is HIGH\n\n' +
-                    '  --swing        ON / OFF <string>\n' +
-                    '  --sleep        ON / OFF <string>\n' +
-                    '  --turbo        ON / OFF <string>\n' +
-                    '  --quiet        ON / OFF <string>\n' +
-                    '  --light        ON / OFF <string>\n')
+        help()
     }
 }
 
